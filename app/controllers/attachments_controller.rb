ed8df4ad755e9437c5bdba2715c3491d6c7b09b7
Txinto Vaz <txinto@gmail.com>
2013-12-06 00:13:57 +0000

    Tutorial done

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -2,7 +2,30 @@ class AttachmentsController < ApplicationController
 
   hobo_model_controller
 
-  auto_actions :write_only
+  auto_actions :all
   auto_actions_for :project, [:create]
 
+  def show
+    attachment = Attachment.find(params[:id])
+    send_file attachment.document.path(params[:style]), :disposition => 'inline'
+  end
+  
+  def create
+    @project = Project.find(params[:project_id])
+    @attachment = @project.attachments.new(params[:attachment])
+    if @attachment.save
+      @this = @project
+      hobo_ajax_response
+    else
+      render :js => "alert('Error while saving record: #{@attachment.errors.full_messages.to_sentence}');"
+    end
+  end
+  
+  def destroy
+    @attachment = Attachment.find(params[:id])
+    if @attachment.destroy
+      render :js => "$('#attachment_#{@attachment.id}').fadeOut();"
+    end
+  end  
+  
 end
--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -10,6 +10,29 @@ class Attachment < ActiveRecord::Base
   
   belongs_to :project, :inverse_of => :attachments, :creator => :true
   
+  has_attached_file :document,
+    :styles => {
+    :medium => ["200x138#", :png],
+    :thumb => ["100x100>", :png] },
+    :whiny => false,
+    :path => 'lib/attachments/:style/:id-:filename',
+    :url => '/attachments/:id.:extension?style=:style'
+  validates_attachment_size :document, { :in => 0..1500.kilobytes }
+  
+  def thumbnail_url(style = :original)
+    if style == :original || FileTest.exists?(document.path(style))
+      document.url(style)
+    else
+      '/assets/text-file-icon.png'
+    end
+  end
+  
+  def short_name
+    splitted = document.original_filename.split('.')
+    return "#{splitted[0].first(18)}.#{splitted[1]}"+
+      " (#{I18n.l created_at.to_date})"
+  end
+    
   # --- Permissions --- #
 
   def create_permitted?
